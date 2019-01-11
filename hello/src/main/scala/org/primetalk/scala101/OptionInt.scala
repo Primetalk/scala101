a6f9f1f037d81c0ae7bc8b66f603ab42874c113d
primetalk <zhizhelev@primetalk.ru>
2019-01-11 23:23:04 +0300

    Add `MyOption` with some operations (map, flatMap)

--- hello/src/main/scala/org/primetalk/scala101/OptionInt.scala
+++ hello/src/main/scala/org/primetalk/scala101/OptionInt.scala
@@ -3,3 +3,31 @@ package org.primetalk.scala101
 sealed trait OptionInt
 case object NoneInt extends OptionInt
 case class SomeInt(i: Int) extends OptionInt
+
+sealed trait MyOption[+T]
+case object MyNone extends MyOption[Nothing]
+case class MySome[T](value: T) extends MyOption[T]
+
+object MyOption {
+  def map[A, B](opt: MyOption[A])(f: A => B): MyOption[B] = {
+    opt match {
+      case MyNone => MyNone
+      case MySome(a) => MySome(f(a))
+    }
+  }
+
+//  val o: Option[Int]
+//  o.map(_ + 1)
+  def flatMap[A, B](opt: MyOption[A])(f: A => MyOption[B]): MyOption[B] = {
+    opt match {
+      case MyNone => MyNone
+      case MySome(a) => f(a)
+    }
+  }
+
+  def fold[A, B](opt: MyOption[A], z: B)(f: A => B): B =
+    opt match {
+      case MyNone => z
+      case MySome(a) => f(a)
+    }
+}
